2ceeb152e6fb52da8bc3539311df7c367870b1f2
Benjamin Philippot <153378698+benPhilippot@users.noreply.github.com>
2025-09-25 11:10:51 +0200

    Create Draw_Line_Between_Objects.cs
    add script

---
+++ Draw_Line_Between_Objects.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Draw_Line_Between_Objects : MonoBehaviour
+{
+    [SerializeField] private LineRenderer line;
+    [SerializeField] private GameObject ObjectA;
+    [SerializeField] private GameObject ObjectB;
+
+    void OnDrawGizmos()
+    {
+        updateLineRendererPosition();
+    }
+
+    void Update()
+    {
+        updateLineRendererPosition();
+    }
+
+    void updateLineRendererPosition()
+    {
+        if (line == null || ObjectA == null || ObjectB == null)
+            return;
+
+        line.SetPosition(0, ObjectA.transform.position);
+
+        line.SetPosition(1, ObjectB.transform.position);
+    }
+}
